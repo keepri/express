d657ee0198db3d92394e793fb51bbce6a9fb28f3
KIPRI <hi@kipri.dev>
2023-08-18 11:17:54 +0300

    renamed libsql createClient variable

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,8 +3,8 @@ import { drizzle } from "drizzle-orm/libsql";
 import * as schema from "./schema.js";
 import env from "../utils/env.js";
 
-const connection = createClient({
+const client = createClient({
     url: env.DATABASE_URL,
     authToken: env.DATABASE_AUTH_TOKEN,
 });
-export const db = drizzle(connection, { schema, logger: true });
+export const db = drizzle(client, { schema, logger: true });
